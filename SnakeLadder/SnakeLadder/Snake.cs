58f7bea958f4d6cba0238c30ae96e52f31aa180b
kavyabs11 <kavyasomshekar11@gmail.com>
2022-08-25 08:35:03 +0530

    [kavya] Add: Check for a Option

--- SnakeLadder/SnakeLadder/Snake.cs
+++ SnakeLadder/SnakeLadder/Snake.cs
@@ -8,14 +8,41 @@ namespace SnakeLadder
 {
     public class Snake
     {
+        public const int ladder = 1, snake = 2;
         public void Ladder()
         {
-            int position = 0;
-            Console.WriteLine($"The starting position of player is {position}");
+            int position1 = 0;
+            Console.WriteLine($"The starting position of player is {position1}");
+            Console.WriteLine("\n");
             Random random = new Random();
             int dice = random.Next(1, 7);
             Console.WriteLine($"The number got by dice roll is {dice}");
+            Random random2 = new Random();
+            int option = random2.Next(0, 3);
+            switch (option)
+            {
+                case ladder:
+                    position1 += dice;
+                    Console.WriteLine("The player climbed the ladder");
+                    break;
+                case snake:
+                    position1 -= dice;
+                    if (position1 < 0)
+                    {
+                        Console.WriteLine("The player bit by snake");
+                        position1 = 0;
+                    }
+                    else
+                    {
+                        Console.WriteLine("The player bit by snake");
+                    }
+                    break;
+                default:
+                    Console.WriteLine($"No play");
+                    break;
+            }
+            Console.WriteLine($"The player position is {position1}");
+            Console.WriteLine("\n");
         }
     }
 }
-
