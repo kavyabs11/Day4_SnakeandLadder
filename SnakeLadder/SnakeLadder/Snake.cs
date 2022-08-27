350e9bdbaa3ec1d2ff119592a7241d61c86924da
kavyabs11 <kavyasomshekar11@gmail.com>
2022-08-27 08:13:55 +0530

    [kavya] Add: Adding two player

--- SnakeLadder/SnakeLadder/Snake.cs
+++ SnakeLadder/SnakeLadder/Snake.cs
@@ -9,52 +9,41 @@ namespace SnakeLadder
     public class Snake
     {
         public const int ladder = 1, snake = 2;
-        public void Ladder()
+        internal int option;
+        internal int position = 0, dice = 0, roll = 0;
+
+        public void Game()
         {
-            int position1 = 0;
-            int dice = 0;
-            int roll = 0;
-            Console.WriteLine($"The starting position of player is {position1}");
-            Console.WriteLine("\n");
-            do
+            roll++;
+            Random random = new Random();
+            dice = random.Next(1, 7);
+            Console.WriteLine($"The number got by dice roll is {dice}");
+            option = random.Next(0, 3);
+            switch (option)
             {
-                roll++;
-                Random random = new Random();
-                dice = random.Next(1, 7);
-                Console.WriteLine($"The number got by dice roll is {dice}");
-                Random random2 = new Random();
-                int option = random2.Next(0, 3);
-                switch (option)
-                {
-                    case ladder:
-                        position1 += dice;
-                        Console.WriteLine("The player climbed the ladder");
-                        break;
-                    case snake:
-                        position1 -= dice;
-                        if (position1 < 0)
-                        {
-                            Console.WriteLine("The player bit by snake");
-                            position1 = 0;
-                        }
-                        else
-                        {
-                            Console.WriteLine("The player bit by snake");
-                        }
-                        break;
-                    default:
-                        Console.WriteLine($"No play");
-                        break;
-                }
-                if (position1 > 100)
-                {
-                    position1 = 0;
-                }
-                Console.WriteLine($"The player position is {position1}");
-                Console.WriteLine($"The dice has been rolled {roll} times till now");
-                Console.WriteLine("\n");
-            } while (position1 < 100);
-            Console.WriteLine($"The dice has been rolled {roll} times to reach 100");
+                case ladder:
+                    position += dice;
+                    Console.WriteLine("The player climbed the ladder");
+                    break;
+                case snake:
+                    position -= dice;
+                    if (position < 0)
+                    {
+                        Console.WriteLine("The player bit by snake");
+                        position = 0;
+                    }
+                    else
+                    {
+                        Console.WriteLine("The player bit by snake");
+                    }
+                    break;
+                default:
+                    Console.WriteLine($"No play for player");
+                    break;
+            }
+            Console.WriteLine($"The player position is {position}");
+            Console.WriteLine($"The dice has been rolled {roll} time by player till now");
+            Console.WriteLine("\n");
         }
     }
 }
